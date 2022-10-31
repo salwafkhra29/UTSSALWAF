d17dd0bef4e17ff1fe915803c21fd376f9b202bf
matthew vincetius <112602374+salwafkhra29@users.noreply.github.com>
2022-10-31 14:50:43 +0700

    second commit

--- app/src/main/java/com/si5a/utssalwaf/MainActivity.java
+++ app/src/main/java/com/si5a/utssalwaf/MainActivity.java
@@ -3,8 +3,13 @@ package com.si5a.utssalwaf;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnDaftar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,7 +17,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnDaftar = findViewById(R.id.btn_daftar);
-        btnDaftar
+        btnDaftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Anda Sudah Terdaftar!", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
